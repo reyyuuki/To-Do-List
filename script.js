43e52114069225e7abc48d8cff667300ef331690
reyyuuki <160479695+reyyuuki@users.noreply.github.com>
2024-05-25 22:44:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ const Delete = document.getElementsByClassName("Delete");
 
 List.innerHTML = "";
 let index = 0;
-let selected = false;
 
 function create() {
     if (Input.value == "") {
@@ -21,41 +20,39 @@ function create() {
 
 
         for (let i = 0; i < CheckBox.length; i++) {
-            if (!selected) {
-                CheckBox[i].addEventListener('click', function () {
-                    const check = this;
-                    if (!selected) {
-                        check.style.background = "orange";
-                        check.style.color = "white";
-                        words[i].style.textDecoration = "line-through";
-                        words[i].style.color = "green";
-                        task[i].style.opacity = "0.5";
-                        selected = true;
-                        index++;
-                    }
-                });
-            } else {
-                selected = false;
-            }
+
+            CheckBox[i].addEventListener('click', function () {
+                const check = this;
+                if (index == 0) {
+                    check.style.background = "green";
+                    check.style.color = "white";
+                    words[i].style.textDecoration = "line-through";
+                    words[i].style.color = "green";
+                    task[i].style.opacity = "0.5";
+                    index++;
+                }
+                else if (index == 1) {
+                    check.style.background = "none";
+                    words[i].style.textDecoration = "none";
+                    words[i].style.color = "white";
+                    task[i].style.opacity = "1";
+                    index--;
+                }
+
+            });
         }
 
-        if (!selected) {
-            for (let i = 0; i < Delete.length; i++) {
-                Delete[i].addEventListener('click', function () {
-                    const button = this;
+        for (let i = 0; i < Delete.length; i++) {
+            Delete[i].addEventListener('click', function () {
+                const button = this;
+                button.style.backgroundColor = "red";
+                setTimeout(() => {
+                    const taskToRemove = button.parentElement;
+                    taskToRemove.remove();
+                }, 200);
 
-                    button.style.backgroundColor = "red";
-                    setTimeout(() => {
-                        const taskToRemove = button.parentElement;
-                        taskToRemove.remove();
-                    }, 200);
-                });
-            }
+            });
         }
-        else {
-            selected = false;
-        }
-
 
     }
 }
